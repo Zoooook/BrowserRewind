c4d5baf81e9c78c97a89003dc3943ba1cdd05d7b
Zoooook <zzzzzzooooook@gmail.com>
2015-01-20 17:48:45 -0600

    Wtf git

--- options.js
+++ options.js
@@ -1,3 +1,12 @@
+// make better interface than timestamp input
+// disable event logging while recreating
+// option to clear history
+// delete old events/snapshots
+
+var pendingBrowserState = {};
+var tempBrowserState = {};
+var testdisable;
+
 $(function(){
     displayVariables();
 
@@ -35,28 +44,111 @@ $(function(){
     });
 });
 
+function setRecreateButtonEvent(){
+    $("#recreatebutton").on('click',function(){
+        chrome.tabs.query({},function(tabs){
+            tempBrowserState={};
+            tempBrowserState.windows={};
+            for(var i=0; i<tabs.length; ++i){
+                if(!tabs[i].incognito){
+                    if(!tempBrowserState.windows.hasOwnProperty("window"+tabs[i].windowId)){
+                        tempBrowserState.windows["window"+tabs[i].windowId]={};
+                        tempBrowserState.windows["window"+tabs[i].windowId].id=tabs[i].windowId;
+                        tempBrowserState.windows["window"+tabs[i].windowId].tabs={};
+                    }
+                    tempBrowserState.windows["window"+tabs[i].windowId].tabs["tab"+tabs[i].id]={};
+                    tempBrowserState.windows["window"+tabs[i].windowId].tabs["tab"+tabs[i].id].id=tabs[i].id;
+                    tempBrowserState.windows["window"+tabs[i].windowId].tabs["tab"+tabs[i].id].index=tabs[i].index;
+                    tempBrowserState.windows["window"+tabs[i].windowId].tabs["tab"+tabs[i].id].pinned=tabs[i].pinned;
+                    tempBrowserState.windows["window"+tabs[i].windowId].tabs["tab"+tabs[i].id].url=tabs[i].url;
+                }
+            }
+
+            var pendingChromeWindows=[]; // transfer keys to array so we can use recursion to circumvent forced async shit (as per usual)
+            for(var pendingChromeWindow in pendingBrowserState.windows){
+                pendingChromeWindows.push(pendingChromeWindow);
+            }
+            recreateWindow(pendingChromeWindows, 0);
+        });
+    });
+}
+
+function recreateWindow(pendingChromeWindows, index){
+    chrome.windows.create({},function(chromeWindow){
+        chrome.tabs.query({"windowId": chromeWindow.id},function(emptyTab){
+            for(var i=0; i<Object.keys(pendingBrowserState.windows[pendingChromeWindows[index]].tabs).length; ++i){
+                var tabUrl;
+                for(var tab in pendingBrowserState.windows[pendingChromeWindows[index]].tabs){
+                    if(pendingBrowserState.windows[pendingChromeWindows[index]].tabs[tab].index == i){
+                        tabUrl = pendingBrowserState.windows[pendingChromeWindows[index]].tabs[tab].url;
+                        tabPinned = pendingBrowserState.windows[pendingChromeWindows[index]].tabs[tab].pinned;
+                        break;
+                    }
+                }
+
+                var tabId = -1;
+                for(var tempChromeWindow in tempBrowserState.windows){
+                    for(var tab in tempBrowserState.windows[tempChromeWindow].tabs){
+                        if(tempBrowserState.windows[tempChromeWindow].tabs[tab].url == tabUrl){
+                            tabId = tempBrowserState.windows[tempChromeWindow].tabs[tab].id;
+                            delete tempBrowserState.windows[tempChromeWindow].tabs[tab];
+                            break;
+                        }
+                    }
+                    if(tabId>-1)
+                        break;
+                }
+
+                if(tabId>-1){
+                    chrome.tabs.move(tabId,{"windowId": chromeWindow.id, "index": i});
+                    if(tabPinned)
+                        chrome.tabs.update(tabId,{"pinned": true});
+                }else
+                    chrome.tabs.create({"windowId": chromeWindow.id, "pinned": tabPinned, "url": tabUrl});
+            }
+
+            chrome.tabs.remove(emptyTab[0].id);
+            chrome.tabs.update(pendingBrowserState.windows[pendingChromeWindows[index]].activeTab, {"active": true});
+
+            if(index+1<pendingChromeWindows.length)
+                recreateWindow(pendingChromeWindows, index+1);
+            else
+                for(var tempChromeWindow in tempBrowserState.windows)
+                    chrome.windows.remove(tempBrowserState.windows[tempChromeWindow].id);
+        });
+    });
+}
+
 function displayVariables(){
     chrome.storage.local.get("numSnapshots", function(result){
-        $("#numSnapshots").html("numSnapshots: " + result.numSnapshots);
-    });
+        if(result.numSnapshots==undefined)
+            displayVariables();
+        else{
+            $("#numSnapshots").html("numSnapshots: " + result.numSnapshots);
 
-    chrome.storage.local.get("numEvents", function(result){
-        $("#numEvents").html("numEvents: " + result.numEvents);
-    });
+            chrome.storage.local.get("numEvents", function(result){
+                $("#numEvents").html("numEvents: " + result.numEvents);
+            });
 
-    chrome.storage.local.get("timestamps", function(result){
-        $("#timestamps").html("timestamps:<ul id=timestampArray></ul>");
-        for(var i=0; i<result.timestamps.length; ++i){
-            $("#timestampArray").append("<li>" + result.timestamps[i] + "</li>");
+            chrome.storage.local.get("timestamps", function(result){
+                $("#timestamps").html("timestamps:<ul id=timestampArray></ul>");
+                for(var i=0; i<result.timestamps.length; ++i){
+                    $("#timestampArray").append("<li>" + result.timestamps[i] + "</li>");
+                }
+            });
         }
     });
 }
 
 function displayBrowserState(title, browserState, timestamp, eventIndex){
+    pendingBrowserState.windows = browserState.windows
+
     if(browserState.hasOwnProperty("timestamp"))
-        $("#snapshot").html(title + ":<ul id=\"chromeWindows\"><li>timestamp: " + browserState.timestamp + "</li><p></p><li>eventIndex: " + browserState.eventIndex + "</li><p></p></ul>");
+        $("#snapshot").html(title + ": <input type=\"button\" id=\"recreatebutton\" value=\"Recreate\"/><ul id=\"chromeWindows\"><li>timestamp: " + browserState.timestamp + "</li><p></p><li>eventIndex: " + browserState.eventIndex + "</li><p></p></ul>");
     else
-        $("#snapshot").html(title + ":<ul id=\"chromeWindows\"><li>timestamp: " + timestamp + "</li><p></p><li>eventIndex: " + eventIndex + "</li><p></p></ul>");
+        $("#snapshot").html(title + ": <input type=\"button\" id=\"recreatebutton\" value=\"Recreate\"/><ul id=\"chromeWindows\"><li>timestamp: " + timestamp + "</li><p></p><li>eventIndex: " + eventIndex + "</li><p></p></ul>");
+
+    setRecreateButtonEvent();
 
     for(var chromeWindow in browserState.windows){
         $("#chromeWindows").append("<li>"+chromeWindow+"</li><ul id="+chromeWindow+"></ul><br/>");
